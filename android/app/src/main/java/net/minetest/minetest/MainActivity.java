646f67221222d93633cb0dfd6968ad9d24fe3371
cgantonio387 <cgantonio387@gmail.com>
2026-08-17 09:20:21 +0200

    Integracion AdMob manual completa

--- android/app/src/main/java/net/minetest/minetest/MainActivity.java
+++ android/app/src/main/java/net/minetest/minetest/MainActivity.java
@@ -1,176 +1,110 @@
-/*
-Minetest
-Copyright (C) 2014-2020 MoNTE48, Maksim Gamarnik <MoNTE48@mail.ua>
-Copyright (C) 2014-2020 ubulem,  Bektur Mambetov <berkut87@gmail.com>
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation; either version 2.1 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with this program; if not, see <https://www.gnu.org/licenses/>.
-*/
-
 package net.minetest.minetest;
 
-import android.annotation.SuppressLint;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import androidx.annotation.NonNull;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.OnUserEarnedRewardListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.android.gms.ads.rewarded.RewardItem;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+importcom.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
 
-import androidx.annotation.RequiresApi;
-import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends UnusedActivity {
+    private AdView adView;
+    private InterstitialAd mInterstitialAd;
+    private RewardedAd mRewardedAd;
+    private boolean isAdFree = false;
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
-import static net.minetest.minetest.UnzipService.*;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MobileAds.initialize(this, initializationStatus -> {});
 
-public class MainActivity extends AppCompatActivity {
-	public static final String NOTIFICATION_CHANNEL_ID = "Minetest channel";
-	public static final int NOTIFICATION_ID_UNZIP = 1;
-	public static final int NOTIFICATION_ID_GAME = 2;
+        setupBanner();
+        loadInterstitial();
+        loadRewardedAd();
+        startInterstitialTimer();
+    }
 
-	private final static int versionCode = BuildConfig.VERSION_CODE;
-	private static final String SETTINGS = "MinetestSettings";
-	private static final String TAG_VERSION_CODE = "versionCode";
+    private void setupBanner() {
+        adView = new AdView(this);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/9214589741");
+        adView.setAdSize(AdSize.BANNER);
 
-	private ProgressBar mProgressBar;
-	private TextView mTextView;
-	private SharedPreferences sharedPreferences;
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        params.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
 
-	private final BroadcastReceiver myReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			int progress = 0;
-			@StringRes int message = 0;
-			if (intent != null) {
-				progress = intent.getIntExtra(ACTION_PROGRESS, 0);
-				message = intent.getIntExtra(ACTION_PROGRESS_MESSAGE, 0);
-			}
+        addContentView(adView, params);
+        adView.loadAd(new AdRequest.Builder().build());
+    }
 
-			if (progress == FAILURE) {
-				Toast.makeText(MainActivity.this, intent.getStringExtra(ACTION_FAILURE), Toast.LENGTH_LONG).show();
-				finish();
-			} else if (progress == SUCCESS) {
-				startNative();
-			} else {
-				if (mProgressBar != null) {
-					mProgressBar.setVisibility(View.VISIBLE);
-					if (progress == INDETERMINATE) {
-						mProgressBar.setIndeterminate(true);
-					} else {
-						mProgressBar.setIndeterminate(false);
-						mProgressBar.setProgress(progress);
-					}
-				}
-				mTextView.setVisibility(View.VISIBLE);
-				if (message != 0)
-					mTextView.setText(message);
-			}
-		}
-	};
+    private void loadInterstitial() {
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712",
+            new AdRequest.Builder().build(),
+            new InterstitialAdLoadCallback() {
+                @Override
+                public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                    mInterstitialAd = interstitialAd;
+                }
+            });
+    }
 
-	@SuppressLint("UnspecifiedRegisterReceiverFlag")
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    private void startInterstitialTimer() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!isAdFree && mInterstitialAd != null) {
+                    mInterstitialAd.show(MainActivity.this);
+                    loadInterstitial();
+                }
+                handler.postDelayed(this, 10 * 60 * 1000);
+            }
+        }, 10 * 60 * 1000);
+    }
 
-		IntentFilter filter = new IntentFilter(ACTION_UPDATE);
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-			registerReceiver(myReceiver, filter, RECEIVER_NOT_EXPORTED);
-		} else {
-			registerReceiver(myReceiver, filter);
-		}
+    private void loadRewardedAd() {
+        RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917",
+            new AdRequest.Builder().build(),
+            new RewardedAdLoadCallback() {
+                @Override
+                public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                    mRewardedAd = rewardedAd;
+                }
+            });
+    }
 
-		mProgressBar = findViewById(R.id.progressBar);
-		mTextView = findViewById(R.id.textView);
-		sharedPreferences = getSharedPreferences(SETTINGS, Context.MODE_PRIVATE);
+    public void showRewardedAd() {
+        if (mRewardedAd != null) {
+            mRewardedAd.show(this, new OnUserEarnedRewardListener() {
+                @Override
+                public void onUserEarnedReward(@NonNull RewardItem rewardItem) {
+                    activateAdFreePeriod();
+                }
+            });
+        }
+    }
 
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-			createNotificationChannel();
+    private void activateAdFreePeriod() {
+        isAdFree = true;
+        if (adView != null) adView.setVisibility(View.GONE);
 
-		checkAppVersion();
-	}
-
-	private void checkAppVersion() {
-		if (UnzipService.getIsRunning()) { // when exactly does this happen?
-			Log.i("MainActivity", "unzip is still running");
-			mProgressBar.setVisibility(View.VISIBLE);
-			mProgressBar.setIndeterminate(true);
-			mTextView.setVisibility(View.VISIBLE);
-			return;
-		}
-		int stored = sharedPreferences.getInt(TAG_VERSION_CODE, 0);
-		boolean valid = Utils.isInstallValid(this);
-		Log.i("MainActivity", String.format("found v%d, want v%d, install is %s",
-			stored, versionCode, valid ? "valid" : "invalid"));
-		if (stored == versionCode && valid) {
-			startNative();
-		} else {
-			mProgressBar.setVisibility(View.VISIBLE);
-			mProgressBar.setIndeterminate(true);
-			mTextView.setVisibility(View.VISIBLE);
-
-			Intent intent = new Intent(this, UnzipService.class);
-			startService(intent);
-		}
-	}
-
-	private void startNative() {
-		Log.i("MainActivity", "starting native code");
-		sharedPreferences.edit().putInt(TAG_VERSION_CODE, versionCode).apply();
-		Intent intent = new Intent(this, GameActivity.class);
-		intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-		startActivity(intent);
-	}
-
-	@RequiresApi(Build.VERSION_CODES.O)
-	private void createNotificationChannel() {
-		NotificationManager notifyManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		if (notifyManager == null)
-			return;
-
-		NotificationChannel notifyChannel = new NotificationChannel(
-			NOTIFICATION_CHANNEL_ID,
-			getString(R.string.notification_channel_name),
-			NotificationManager.IMPORTANCE_LOW
-		);
-		notifyChannel.setDescription(getString(R.string.notification_channel_description));
-		// Configure the notification channel without sound set
-		notifyChannel.setSound(null, null);
-		notifyChannel.enableLights(false);
-		notifyChannel.enableVibration(false);
-
-		// It is fine to always create the notification channel because creating a channel
-		// with the same ID is the same as overriding it (only its name and description).
-		notifyManager.createNotificationChannel(notifyChannel);
-	}
-
-	@Override
-	public void onBackPressed() {
-		// Prevent abrupt interruption when copy game files from assets
-	}
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-		unregisterReceiver(myReceiver);
-	}
+        handler.postDelayed(() -> {
+            isAdFree = false;
+            if (adView != null) adView.setVisibility(View.VISIBLE);
+        }, 20 * 60 * 1000);
+    }
 }
